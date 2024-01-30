365f6887be2eb0edc28a788fd732a5b764b87853
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-01-30 06:47:03 +0600

    Create quiz.js

---
+++ Milestone-4 @Hello Javascript/Module 17@Fundamental Concept of Conditionals/Practise Task/quiz.js
@@ -0,0 +1,16 @@
+let x = 5; if (x < 10) { x = x + 3; } else { x = x - 2; } console.log(x);
+
+let result = 10 < "5";
+console.log(result);
+
+
+console.log(6 != 6);
+
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
