960bdd12425087a90502745150d8d04f164fa167
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-01-30 06:51:18 +0600

    Create free drinks.js

---
+++ Milestone-4 @Hello Javascript/Module 17@Fundamental Concept of Conditionals/Practise Task/free drinks.js
@@ -0,0 +1,10 @@
+const burger = 50;
+let cokePrice;
+if (burger > 500){
+    cokePrice = 0;
+    console.log("coke price is = " + cokePrice + " .so its a free Coke");
+}
+else{
+    cokePrice = 30;
+    console.log("coke price = " + cokePrice + " tk");
+}
