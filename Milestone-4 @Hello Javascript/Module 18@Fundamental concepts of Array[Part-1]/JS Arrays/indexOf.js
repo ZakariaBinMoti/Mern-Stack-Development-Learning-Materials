2f832e418d992d6ae58f96942f55dc9732a180c7
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-01 04:38:16 +0600

    Create indexOf.js

---
+++ Milestone-4 @Hello Javascript/Module 18@Fundamental concepts of Array[Part-1]/JS Arrays/indexOf.js
@@ -0,0 +1,9 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+console.log(friends.indexOf('gelam'));
+
+console.log(friends.indexOf('tomato'))
+
+console.log(friends.indexOf(12));
+let a = 1;
+console.log(Array.isArray(a));
