a37fb7664ff22dcd0d2d12583e0584ecdd591bc3
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-05 22:16:47 +0600

    Create shift-unshift.js

---
+++ Milestone-4 @Hello Javascript/Module 18@Fundamental concepts of Array[Part-1]/JS Arrays/shift-unshift.js
@@ -0,0 +1,11 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+console.log(friends);
+friends.shift();
+console.log(friends);
+
+friends.unshift('ghumailam')
+friends.unshift(12)
+console.log(friends)
+
+
+console.log(typeof(friends[0]));
