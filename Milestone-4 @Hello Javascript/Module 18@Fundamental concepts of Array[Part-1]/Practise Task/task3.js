c9028518a5f38c052c20fa5b275afe9e595a47ec
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-07 04:28:34 +0600

    Create task3.js

---
+++ Milestone-4 @Hello Javascript/Module 18@Fundamental concepts of Array[Part-1]/Practise Task/task3.js
@@ -0,0 +1,7 @@
+const books = ['bangla', 'english', 'math'];
+if(books.includes('javascript')){
+    console.log('The element is present in the array');
+}
+else{
+    console.log("The element is not present in the array");
+}
