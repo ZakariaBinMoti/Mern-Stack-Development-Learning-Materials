6aa4b324a6d9a8b9636becee34954b20bc658b27
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-07 04:24:23 +0600

    Create task2.js

---
+++ Milestone-4 @Hello Javascript/Module 18@Fundamental concepts of Array[Part-1]/Practise Task/task2.js
@@ -0,0 +1,7 @@
+const places = ['mohasthangar', 'moynamoti', 'sat gombuj mosjid'];
+places.push('dhaka');
+console.log(places);
+places.push('barishal', 'noakhali');
+console.log(places);
+places.pop();
+console.log(places);
