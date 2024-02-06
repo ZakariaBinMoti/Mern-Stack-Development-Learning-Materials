d1851940780ba2a54836b6f0bd2782280370c4b3
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-07 04:35:37 +0600

    Create task4.js

---
+++ Milestone-4 @Hello Javascript/Module 18@Fundamental concepts of Array[Part-1]/Practise Task/task4.js
@@ -0,0 +1,9 @@
+const var1 = [2, 3, 5346, 2325, 4647, 78, 686, 86, 53];
+const var2 = 242354;
+
+if(Array.isArray(var1) && var1.length==9){
+    console.log("var1 is an array");
+}
+else{
+    console.log("its not an array");
+}
