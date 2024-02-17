023868b8b96692385e9580dfd8076ff0d74ba4cd
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-02-18 05:09:41 +0600

    Create styleIn.js

---
+++ Milestone-5 @Integrate JavaScript (JavaScript in A Relation)/Module 24@Tour of DOM (Document Object Model)/tour-of-dom/js/styleIn.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.backgroundColor = 'skyblue';
+    section.style.paddingLeft = '10px';
+}
+
+const section = document.getElementById('fruits-list');
+section.classList.add('bg-color');
